81957790dd16cfbc32ae7f0cac0638626e0f74df
cloneman666 <1171836398@qq.com>
2020-10-24 22:15:35 +0800

    change some

--- Seq2Seq/test.py
+++ Seq2Seq/test.py
@@ -1,5 +1,36 @@
 import pandas as pd
 import numpy as np
 
-data = pd.read_csv()
+# data = pd.read_csv()
 #这个版本是主版本
+
+class Config(object):
+    def __init__(self):
+        self.model_name = 'CNN-LSTM'
+
+        self.inputSize = 255
+        self.hiddenSize = 2
+
+
+
+
+class Encoder():
+    def __init__(self,config):
+        super(Decoder, self).__init__()
+        self.input_dim = config.inputSize
+        self.hidden_dim = config.hiddenSize
+
+
+    def forward(self):
+        pass
+
+class Decoder():
+    def __init__(self):
+        super(Decoder, self).__init__()
+
+    def forward(self):
+        pass
+
+class Seq2Seq():
+    def __init__(self,encoder,decoder):
+        super(Seq2Seq, self).__init__()
